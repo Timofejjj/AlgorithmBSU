d45f0ec96eba5531d5a2c069f3020b82c3adfea1
Timofejjj <157852282+Timofejjj@users.noreply.github.com>
2025-03-03 13:21:52 +0300

    Update code_2.cpp

--- 07/code_2.cpp
+++ 07/code_2.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <cstdint>
 #include <algorithm>
 
 using namespace std;
 
 int main() {
-    int N, M;
+    size_t N, M;
     cin >> N >> M;
 
-    vector<vector<long long>> arr(N, vector<long long>(M));
+    vector<vector<int64_t>> arr(N, vector<int64_t>(M));
     
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < M; j++) {
             cin >> arr[i][j];
         }
     }
 
-    long long max_sum = 0;
+    int64_t max_sum = 0;
 
-    for (int top = 0; top < N; top++) {
-        vector<long long> temp(M, 0);
+    for (size_t top = 0; top < N; top++) {
+        vector<int64_t> temp(M, 0);
     
-        for (int bottom = top; bottom < N; bottom++) {
+        for (size_t bottom = top; bottom < N; bottom++) {
 
-            for (int j = 0; j < M; j++) {
+            for (size_t j = 0; j < M; j++) {
                 temp[j] += arr[bottom][j];
             }
 
-            long long current = 0, current_max = 0;
-            for (int x : temp) {
+            int64_t current = 0, current_max = 0;
+            for (auto&& x : temp) {
                 current = max(0LL, current + x);
                 current_max = max(current_max, current);
             }

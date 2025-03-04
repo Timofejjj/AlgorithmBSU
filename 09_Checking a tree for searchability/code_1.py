30c340da695645ac1e5559f8e4beb3af9e967f2d
Timofejjj <157852282+Timofejjj@users.noreply.github.com>
2025-03-04 23:27:18 +0300

    Update code_1.py

--- 09_Checking a tree for searchability/code_1.py
+++ 09_Checking a tree for searchability/code_1.py
@@ -1,119 +1,72 @@
-##-----Without list
-
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-
-    def insert(self, key):
-        if key < self.key:
-            if self.left is None:
-                self.left = Node(key)
-            else:
-                self.left.insert(key)
-        elif key > self.key:
-            if self.right is None:
-                self.right = Node(key)
-            else:
-                self.right.insert(key)
-
-    def pre_order_traversal(self):
-
-        #реализация прямого левого обхода
-        result = [self.key]
-        if self.left:
-            result += self.left.pre_order_traversal()
-        if self.right:
-            result += self.right.pre_order_traversal()
-        return result
-
-    def search_min_left_key_Adres(self):
-        current = self
-        while current.left:
-            current = current.left
-        return current
-
-    def delite(self, key):
-        if key < self.key:
-            if self.left:
-                self.left = self.left.delite(key)
-            return self
-        elif key > self.key:
-            if self.right:
-                self.right = self.right.delite(key)
-            return self
-        else:
-            if (self.left is None) and (self.right is None):
-                return None
-            if self.right is None:
-                return self.left
-            if self.left is None:
-                return self.right
-
-            most_left_node = self.right.search_min_left_key_Adres()
-            self.key = most_left_node.key
-            self.right = self.right.delite(most_left_node.key)
-            return self
+import sys
+import math
 
 def main():
-    key_to_delete = int(input().strip())
-    input()  
-    tree_keys = []
+ 
+    # low и hight - задаём границы нешему промежутку (основной алгоритм)
+    def cheaking_for_search(i, low, hight):
+        if i == -1:
+            return True
+       if (nodeValues[i] < minBounds[i]) or (nodeValues[i] >= maxBounds[i]):
+            return False
+        return cheaking_for_search(left[i], low, values[i]) and cheaking_for_search(right[i], values[i], hight)
 
+    ###############################
+
+    data = []
     while True:
-        first_line = input().strip()
-        if first_line != "":
-            tree_keys.append(int(first_line))
-        else:
+        user_input = input().strip()  
+        if user_input == "": 
             break
 
-    root = Node(tree_keys[0])
-    for key in tree_keys[1:]:
-        root.insert(key)
-    
-    root = root.delite(key_to_delete)
-    
-    result = root.pre_order_traversal() if root else []
-    print("\n".join(map(str, result)))
+        data.extend(user_input.split())
+     
+    n = int(data[0])
+
+    values = [0] * n
+    left = [-1] * n
+    right = [-1] * n
+
+    values[0] = int(data[1])
+    index = 2
+
+    #Заполнение данных
+    for i in range(1, n):
+        val = int(data[index])
+        parent_index = int(data[index + 1]) - 1
+        direction = data[index + 2]
+        index += 3
+
+        values[i] = val
+        if direction == "L":
+            left[parent_index] = i
+        elif direction == "R":
+            right[parent_index] = i
+
+    # Вызываем функцию
+    if cheaking_for_search(0, -math.inf, math.inf):
+        print("YES")
+    else:
+        print("NO")
 
 if __name__ == '__main__':
     main()
 
-########################################
-
-# if self.left is None and self.right is None:
-#                 return None - вернули для предыдущей ноды "Ничего"
-
-#             # Вариант 2: у вершины только один потомок
-#             if self.left is None: - если слева ничего нет
-
-#                 return self.right - вернули для предыдущей ноды последующую парвую
-
-#             if self.right is None: - если справа ничего нет
-#                 return self.left - вернули для предыдущей ноды  последующую левую
-
 
 #######################################
 
-# while current.left: - до тех пор, пока можем идти влево - идем влево и сохраняем в corrent видимо ссылки. А потом вывродим
-#             current = current.left
-#         return current
+# values[i] = val
+# if child_dir == "L":
+#     left[parent_index] = i - вот тут разобраться с идеей, то есть что
+                                # даёт такое хранение
+# elif child_dir == "R":
+#     right[parent_index] = i
 
+############################# Разбор, то есть меня интерисует как организованы left и right, что мы можнем так писать: 
+# return check_bst(left[i], low, values[i]) and check_bst(right[i], values[i], high) - 
 
-#######################################
+#############################
 
-#Код ниже можно считать стнадартным алгоритмом поиска элемента
-# def search(self, key):
-#         if key < self.key:
-#             if self.left:
-#                 self.left = self.left.delete(key)
+#     data.extend(user_input.split())
 
-#         elif key > self.key:
-#             if self.right:
-#                 self.right = self.right.delete(key)
-
-#         else:
-#           #что то сделать с найденным элементом
-
-#####################################
+#     .extend() - так же как и .append, но в качестве аргумента принимает итератор (итератор, итерируемый объект) и объединяет его со списком

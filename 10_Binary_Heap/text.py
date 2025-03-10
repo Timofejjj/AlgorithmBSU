3b5e011d22303f8d9ceefe76bda383227f29f6d0
Timofejjj <157852282+Timofejjj@users.noreply.github.com>
2025-03-10 18:53:11 +0300

    Add files via upload

---
+++ 10_Binary_Heap/text.py
@@ -0,0 +1,29 @@
+
+# with open('input.txt', 'r') as file:
+#     n = int(file.readline())
+#     arr = np.array([n], dtype=np.int32)
+#     arr = np.fromstring(file.readline().strip(), sep=' ', dtype=np.int32)
+
+n = int(input().strip())
+arr = np.fromstring(input().strip(), sep=' ', dtype=np.int32)
+
+result = "Yes"
+
+for i in range(n):
+
+  if (2*i + 1) < (n - 1) and arr[i] > arr[2*i + 1]:
+    result = "No"
+    break
+
+  if (2*i + 2) < (n - 1) and arr[i] > arr[2*i + 2]:
+    result = "No"
+    break
+
+
+print(result)
+
+# with open('output.txt', 'w') as file:
+#   file.write(result)
+
+#10
+#9 17 16 25 21 20 60 25 40 33
